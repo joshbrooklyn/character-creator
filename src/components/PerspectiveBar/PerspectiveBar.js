b3ab03e69406ac86bb79213610c6fd418c523635
Josh Howe <josh.w.howe@gmail.com>
2023-10-23 21:07:41 -0400

    Modified   src/components/CharacterEditor/CharacterEditor.module.css

---
+++ src/components/PerspectiveBar/PerspectiveBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './PerspectiveBar.module.css';
+
+const perspectiveBar = () => {
+  return (
+    <perspectiveBar className={styles.perspectiveBar} />
+  );
+};
+
+export default perspectiveBar;
